31dc64ab4711d6342f501b2550cf2d5634e79395
roma-bashtevych <roma_bashtevich@ukr.net>
2021-08-19 17:51:17 +0300

    homework2

---
+++ database/users.js
@@ -0,0 +1,8 @@
+module.exports = [
+    {"login":"vitya","password":1111,"email":"vit6ok@ukr.net","age":25},
+    {"login":"alevtina","password":1234,"email":"stepanivna@gmail.com","age":60},
+    {"login":"vasya","password":"4444","email":"vasya_ch@ukr.net","age":"29"},
+    {"login":"katya","password":"0202","email":"katya_cool@pes.ua","age":"18"},
+    {"login":"jacki","password":"0000","email":"jacki_jan@gmail.com","age":"72"},
+    {"login":"silvestr","password":"9999","email":"silvestr_v_stalovoy@ukr.net","age":"14"}
+]
---
+++ index.js
@@ -0,0 +1,70 @@
+// Підключамо модулі
+const express = require('express');
+const expressHbs = require('express-handlebars');
+const path = require('path');
+const fs = require('fs')
+
+// Виносимо змінні в окремі файли
+const {PORT} = require('./config/var')
+const users = require('./database/users')
+
+const userPath = path.join(__dirname, 'database', 'users.js');
+// Налаштовуємо -express для роботи з json;  -handlebars
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'static')))
+app.set('view engine', '.hbs');
+app.engine('.hbs', expressHbs({defaultLayout: false}))
+app.set('views', path.join(__dirname, 'static'));
+
+// метод get на стартову сторінку логінації
+app.get('/', (req, res) => {
+    res.render('login')
+});
+
+app.get('/registration', (req, res) => {
+    res.render('registration');
+})
+// передаємо опшинами масив який буде відображатись на сторінці
+app.get('/users', (req, res) => {
+    res.render('users', {title: 'users', users})
+})
+
+// динамічно змінюємо user_id залежно від місця юзера в масиві
+app.get('/users/:user_id', (req, res) => {
+    const {user_id} = req.params;
+    const countUser = users[user_id]
+    if (!countUser) {
+        res.status(404).end('User Not Found')
+    }
+    res.render('user', {countUser})
+})
+
+app.post('/registration', (req, res) => {
+    let newUser = users.find(user => {
+        return user.login === req.body.login && user.password === +req.body.password
+    })
+     if (!newUser) {
+        users.push(req.body);
+        fs.writeFile(userPath, `module.exports = ${JSON.stringify(users)}`,err => console.log(err))
+        res.redirect('/users')
+        return
+    }
+    res.render('/login')
+})
+
+app.post('/login', (req, res) => {
+    let userIndex = users.findIndex(user => {
+        return user.login === req.body.login && +user.password === +req.body.password
+    })
+    if (userIndex !== -1) {
+        res.redirect(`/users/${userIndex}`)
+        return
+    }
+    res.redirect('/registration')
+})
+
+app.listen(PORT, () => {
+    console.log(`app listen port ${PORT}`)
+})
